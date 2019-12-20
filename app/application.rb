bcc3aaf3d9ea56c202911694085753825e3901b7
Roshanak Agahi <rosh.agahi@gmail.com>
2019-12-20 21:58:33 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,27 @@
+class Application 
+  
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+ 
+    if req.path.match("/items")
+      item = req.path.split("/items/").last
+      search_item = @@items.find { |i| i.name == item }
+      if search_item
+        resp.write search_item.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+      
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    
+    end
+ 
+    resp.finish
+  end
+
+    
+end
