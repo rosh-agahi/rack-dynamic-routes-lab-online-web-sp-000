33c54291a607e8ed09b8b3e72126792a54525a91
Roshanak Agahi <rosh.agahi@gmail.com>
2019-12-20 21:53:22 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -20,5 +20,17 @@ class Application
     resp.finish
   end
 
-  
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+ 
+    if req.path=="/songs"
+      resp.write "You requested the songs"
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+ 
+    resp.finish
+  end
 end
